9d6ca55522bd3de5d8145aefd8af7df50d1042fa
Lucho Gizdov <olebg01@gmail.com>
2017-08-23 18:43:40 +0300

    add navbar to home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,21 @@
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './users/login/login.component';
+import { ListRestaurantsComponent } from './restaurants/list-restaurants/list-restaurants.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home',
-  component: HomeComponent
- },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '#restaurants',
+    component: ListRestaurantsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
 ];
 
 export const AppRoutes = RouterModule.forRoot(routes);
