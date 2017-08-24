d81c326d0b282f2e726879218576525510acbf25
Daniel <didexe@gmail.com>
2017-08-24 08:29:40 +0300

    user model created

---
+++ src/app/models/user.ts
@@ -0,0 +1,43 @@
+import { UserValidatorService } from '../services/userValidator.service';
+export class User {
+    private _id: string;
+    private _username: string;
+    private _firstname: string;
+    private _lastname: string;
+    private _email: string;
+
+
+    constructor(private validator: UserValidatorService, id, username, firstname, lastname, email) {
+        this.validator = validator;
+   }
+
+    set id(id) {
+        this.validator.validateId(id);
+        this._id = id;
+    }
+    get id() {
+        return this._id;
+    }
+    set username(username) {
+        this.validator.validateUsername(username);
+        this._username = username;
+    }
+    set firstname(firstname) {
+        this.validator.validateFirstName(firstname);
+        this._firstname = firstname;
+    }
+    get firstname() {
+        return this._firstname;
+    }
+    set lastname(lastname) {
+        this.validator.validateLastName(lastname);
+        this._firstname = lastname;
+    }
+    get lastname() {
+        return this._lastname;
+    }
+    set email(email) {
+        this.validator.validateEmail(email);
+        this._email = email;
+    }
+}
---
+++ src/app/services/userValidator.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { UserValidatorService } from './userValidator.service';
+
+describe('Service: UserValidator', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UserValidatorService]
+    });
+  });
+
+  it('should ...', inject([UserValidatorService], (service: UserValidatorService) => {
+    expect(service).toBeTruthy();
+  }));
+});
