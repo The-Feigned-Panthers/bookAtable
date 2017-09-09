1aa71d894478dda68c5f4854939fa4c9b47957aa
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-09-09 20:32:37 +0300

    Add review component tests

--- src/app/restaurants/details-restaurant/reviews/reviews.component.spec.ts
+++ src/app/restaurants/details-restaurant/reviews/reviews.component.spec.ts
@@ -1,3 +1,5 @@
+import { User } from './../../../models/user';
+import { Review } from './../../../models/review';
 import { UserService } from './../../../core/services/user.service';
 import { Observable } from 'rxjs/Observable';
 import { Restaurant } from './../../../models/restaurant';
@@ -14,6 +16,13 @@ import { ReviewsComponent } from './reviews.component';
 describe('ReviewsComponent', () => {
   let component: ReviewsComponent;
   let fixture: ComponentFixture<ReviewsComponent>;
+  let debugElement: DebugElement;
+  let element: HTMLElement;
+
+  const restaurant = new Restaurant('Test',
+  { city: 'city', area: 'area', street: 'street', number: '15' },
+  'Bistro', 14, '09:00-23:00', '09:00-23:00', '+3598888888', 'details', 'owner');
+
   const serviceMock = {
     updateRestaurant(rest) {
     }
@@ -39,13 +48,51 @@ describe('ReviewsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReviewsComponent);
     component = fixture.componentInstance;
-    component.restaurant = new Restaurant('Test',
-    { city: 'city', area: 'area', street: 'street', number: '15' },
-    'Bistro', 14, '09:00-23:00', '09:00-23:00', '+3598888888', 'details', 'owner');
+    component.restaurant = restaurant;
+    component.restaurant.reviews.push(new Review(new User('user', 'fName', 'lName', 'email', 'owner'), 'Hi!'));
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    component.restaurant.reviews = [];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render average rating correctly', () => {
+    debugElement = fixture.debugElement.query(By.css('h2'));
+    element = debugElement.nativeElement;
+    expect(element).toBeDefined();
+    expect(element.textContent).toContain(restaurant.rating.average.toPrecision(3));
+  });
+
+  it('should render all reviews of restaurant correctly', () => {
+    debugElement = fixture.debugElement.query(By.css('.review-info'));
+    element = debugElement.nativeElement;
+
+    expect(element.classList.length).toEqual(restaurant.reviews.length);
+  });
+
+  it('should render username of review correctly', () => {
+    debugElement = fixture.debugElement.query(By.css('.review-info'));
+    element = debugElement.nativeElement;
+    expect(element).toBeDefined();
+    expect(element.textContent).toContain(restaurant.reviews[0].user.username);
+  });
+
+  it('should render date of review correctly', () => {
+    debugElement = fixture.debugElement.query(By.css('.review-info'));
+    element = debugElement.nativeElement;
+    expect(element).toBeDefined();
+    expect(element.textContent).toContain(restaurant.reviews[0].date);
+  });
+
+  it('should render message of review correctly', () => {
+    debugElement = fixture.debugElement.query(By.css('.review-message'));
+    element = debugElement.nativeElement;
+    expect(element).toBeDefined();
+    expect(element.textContent).toContain(restaurant.reviews[0].message);
   });
 });
